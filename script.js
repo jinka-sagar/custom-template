1971dd6525763a8fddd5631855eb4e50fdc5701a
jinka-sagar <sagarjinka12@gmail.com>
2024-09-14 20:04:11 +0530

    mywebpage

---
+++ script.js
@@ -0,0 +1,123 @@
+function fun1(){
+    document.getElementById('block1').style.backgroundColor="#47994e";
+}
+function fun2(){
+    document.getElementById('block1').style.backgroundColor="#be0665";
+}
+function fun3(){
+    document.getElementById('block1').style.backgroundColor="#fe2d03";
+}
+function fun4(){
+    document.getElementById('block1').style.backgroundColor="#94f00a";
+}
+function fun5(){
+    document.getElementById('block1').style.backgroundColor="#ea0bd4";
+}
+function fun6(){
+    document.getElementById('block1').style.backgroundColor="#f36709";
+}
+function fun7(){
+    document.getElementById('block1').style.backgroundColor="#18ceef";
+}
+function fun8(){
+    document.getElementById('block1').style.backgroundColor="#B2BEB5";
+}
+function fun9(){
+    document.getElementById('block1').style.backgroundColor="#007FFF";
+}
+function fun10(){
+    document.getElementById('block1').style.backgroundColor="#89CFF0";
+}
+function fun11(){
+    document.getElementById('block1').style.backgroundColor="#bd0948";
+}
+function fun12(){
+    document.getElementById('block1').style.backgroundColor="#848482";
+}
+function fun13(){
+    document.getElementById('block1').style.backgroundColor="#e3c608";
+}
+function fun14(){
+    document.getElementById('block1').style.backgroundColor="#A57164";
+}
+function fun15(){
+    document.getElementById('block1').style.backgroundColor="#0018A8";
+}
+function fun16(){
+    document.getElementById('block1').style.backgroundColor="#FFAA1D";
+}
+function fun17(){
+    document.getElementById('block1').style.backgroundColor="#f00bf0";
+}
+function fun18(){
+    document.getElementById('block1').style.backgroundColor="#BDB76B";
+}
+function fun19(){
+    document.getElementById('block1').style.backgroundColor="#483D8B";
+}
+function fun20(){
+    document.getElementById('block1').style.backgroundColor="#ff00a2";
+}
+function clean(){
+    document.getElementById('block1').style.backgroundColor="#ffffff";
+}
+function fun01(){
+    document.getElementById('textBox').style.color="#47994e";
+}
+function fun02(){
+    document.getElementById('textBox').style.color="#ea0bd4";
+}
+function fun03(){
+    document.getElementById('textBox').style.color="#fe2d03";
+}
+function fun04(){
+    document.getElementById('textBox').style.color="#94f00a";
+}
+function fun05(){
+    document.getElementById('textBox').style.color="#ea0bd4";
+}
+function fun06(){
+    document.getElementById('textBox').style.color="#f36709";
+}
+function fun07(){
+    document.getElementById('textBox').style.color="#18ceef";
+}
+function fun08(){
+    document.getElementById('textBox').style.color="#B2BEB5";
+}
+function fun09(){
+    document.getElementById('textBox').style.color="#007FFF";
+}
+function fun010(){
+    document.getElementById('textBox').style.color="#89CFF0";
+}
+function fun011(){
+    document.getElementById('textBox').style.color="#bd0948";
+}
+function fun012(){
+    document.getElementById('textBox').style.color="#848482";
+}
+function fun013(){
+    document.getElementById('textBox').style.color="#e3c608";
+}
+function fun014(){
+    document.getElementById('textBox').style.color="#A57164";
+}
+function fun015(){
+    document.getElementById('textBox').style.color="#0018A8";
+}
+function fun016(){
+    document.getElementById('textBox').style.color="#FFAA1D";
+}
+function fun017(){
+    document.getElementById('textBox').style.color="#f00bf0";
+}
+function fun018(){
+    document.getElementById('textBox').style.color="#BDB76B";
+}
+function fun019(){
+    document.getElementById('textBox').style.color="#483D8B";
+}
+function fun020(){
+    document.getElementById('textBox').style.color="#ff00a2";
+}
